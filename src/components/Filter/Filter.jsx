8ea19963c878a01a82e20093af91600091f6c2fd
Olena-Vozniuk <110108199+Olena-Vozniuk@users.noreply.github.com>
2023-02-27 01:19:01 +0100

    all

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Input, Label } from './Filter.styled';
+
+export const Filter = ({value, onChange}) => {
+    return (
+        <Label>Find contacts by name
+            <Input type="text" value={value} onChange={onChange} />
+        </Label>
+    )
+};
